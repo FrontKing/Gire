f89c1309707609a8ef7f41b2d9d76b9b3b07b04f
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2017-07-30 21:01:13 +0430

    server made!

---
+++ Server/api/models/userModel.js
@@ -0,0 +1,24 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://<test>:<test>@ds129593.mlab.com:29593/giredb');
+
+var UserSchema = new Schema({
+  name: {
+    type: String,
+    Required: 'شما باید یک نامی برای خود انتخاب کنید',
+     trim : true,
+  },
+  email:{
+      type:String,
+      required : 'شما باید یک ایمیل معتبر برای خود اراعه دهید!',
+      trim : true,
+      unique : true
+  },
+  UserPhoto : {
+      type:String,
+      default : "/images/profile-black.jpg"
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
