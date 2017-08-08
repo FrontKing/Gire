0c4b72ebcc276157f11030b4c23045213929b810
Mohammad Sharifi <mmdsharifi@yahoo.com>
2017-08-09 00:35:50 +0430

    feat(login): +fix register

--- Client/src/app/services/user.service.ts
+++ Client/src/app/services/user.service.ts
@@ -6,25 +6,34 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserService {
-  
+
   private url = "https://gire.herokuapp.com";
   private headers = new Headers({ 'content-type': 'application/json' });
   private options = new RequestOptions({ headers: this.headers });
 
   constructor(private http: Http) { }
-  
-  public register (payload) {
-    return this.http.post(`${this.url}/register`, payload, this.options)
-               .map(res => res.json())
-               .catch(this.handleErrorObservable)
+
+  private post(payload, url: string) {
+    return this.http.post(url, payload, this.options)
+      .map(res => res.json())
+      .catch(this.handleErrorObservable)
   }
+  public register(payload) {
+    return this.post(payload, `${this.url}/register`);
+  }
+
+  public login(payload) {
+    return this.post(payload, `${this.url}/login`)
+  }
+
+
 
   private extractData(res: Response) {
-	  return res.json() || {};
+    return res.json() || {};
   }
 
-  private handleErrorObservable (error: Response | any) {
-  	console.error(error.message || error);
-  	return Observable.throw(error.message || error);
+  private handleErrorObservable(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
   }
 }
--- Client/src/app/user/register/register.component.ts
+++ Client/src/app/user/register/register.component.ts
@@ -1,10 +1,11 @@
+import { Router } from '@angular/router';
 import { UserService } from './../../services/user.service';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { CustomValidators } from 'ng2-validation';
 import { ToastyService, ToastOptions } from "ng2-toasty";
 
-const email = new FormControl('', Validators.compose([Validators.required, CustomValidators.email]));
+const email = new FormControl(' ', Validators.compose([Validators.required, CustomValidators.email]));
 const password = ' ';
 @Component({
   selector: 'app-register',
@@ -24,26 +25,52 @@ export class RegisterComponent implements OnInit {
     })
   }
 
-  constructor(private fb: FormBuilder, private userService: UserService, private toastyService: ToastyService) { }
+  constructor(private fb: FormBuilder, private userService: UserService,
+    private toastyService: ToastyService
+    , private router: Router) { }
 
 
   ngOnInit() {
+
     this.createRegForm()
 
     this.toastyService.wait(<ToastOptions>{
       title: "شکیبا باشید ...",
       msg: "در حال برقراری ارتباط با سرور",
-      theme: "material",
+      theme: "default",
       showClose: true,
       timeout: 5000
     });
   }
 
   onSubmit(value) {
-    console.log(JSON.stringify(value))
+
     this.userService.register(value).subscribe(
       data => {
         console.log(data);
+
+        if (data["status"] == "success") {
+          let msg = data["data"];
+
+          console.log(msg);
+
+          this.toastyService.success({
+            title: "ثبت نام انجام شد",
+            msg: msg[0].message,
+            theme: "default",
+            showClose: true,
+            timeout: 5000
+          })
+          // this.router.navigate(['/login', value["email"]])
+        } else if (data["status"] == "error") {
+          this.toastyService.warning({
+            title: "مجدد تلاش کنید!",
+            msg: data["data"],
+            theme: "default",
+            showClose: true,
+            timeout: 5000
+          })
+        }
       },
       err => console.error(err)
     )
